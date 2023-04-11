257c60b0907fa42ff1ae99e5064d56f734973f64
Rassolchick <113520657+Rassolchick@users.noreply.github.com>
2023-04-11 22:25:48 +0300

    Add files via upload

---
+++ Tests/test_pytest.py
@@ -0,0 +1,43 @@
+import pytest
+from yandex_code import find_country, kill_doubles, make_range, get_token, Yandex
+from data import geo_logs, ids, queries, russia, france, india, func_3_res, func_1_fixture
+
+yandex = Yandex(token='используй свой токен')
+
+
+class Test_country:
+    @pytest.mark.parametrize('check_list, country, etalon', func_1_fixture)
+    def test_find_country(self, check_list, country, etalon):
+        result = find_country(some_list=check_list, country=country)
+        assert result == etalon
+
+    def test_kill_doubles(self):
+        result = kill_doubles(some_dict=ids)
+        standart = [15, 35, 54, 98, 119, 213]
+        assert result == standart
+
+    def test_make_range(self):
+        result = make_range(some_list=queries)
+        standart = func_3_res
+        assert result == standart
+
+class Test_yandex:
+    def test_create_folder(self):
+        result = yandex.create_folder(folder_path='test_folder')
+        etalon = 201
+        assert result == etalon
+
+    def test_delete_folder(self):
+        result = yandex.delete_folder(folder_path='test_folder')
+        etalon = 202
+        assert result == etalon
+
+    def test_wrong_data(self):
+        result = yandex.create_folder(folder_path=None)
+        etalon = 400
+        assert result == etalon
+
+    def test_no_found_folder(self):
+        result = yandex.delete_folder(folder_path='unexisted_folder')
+        etalon = 404
+        assert result == etalon
---
+++ Tests/test_unittest.py
@@ -0,0 +1,26 @@
+import unittest
+from parameterized import parameterized_class, parameterized
+from yandex_code import find_country, kill_doubles, make_range
+from data import geo_logs, ids, queries, russia, france, india, func_3_res, func_1_fixture
+
+
+class TestFunc(unittest.TestCase):
+    def test_find_country(self):
+        result = find_country(some_list=geo_logs, country='россия')
+        etalon = russia
+        self.assertEqual(result, etalon)
+
+    def test_kill_doubles(self):
+        result = kill_doubles(some_dict=ids)
+        standart = [15, 35, 54, 98, 119, 213]
+        self.assertEqual(result, standart)
+
+    def test_make_range(self):
+        result = make_range(some_list=queries)
+        standart = func_3_res
+        self.assertEqual(result, standart)
+
+    @parameterized.expand(func_1_fixture)
+    def test_find_country_2(self, check_list, country, etalon):
+        result = find_country(some_list=check_list, country=country)
+        self.assertEqual(result, etalon)
